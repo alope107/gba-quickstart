e37bccbcef0e60ea8bae25477c7fe5779e5e6545
alope107 <alope107@users.noreply.github.com>
2025-08-23 00:46:36 +0000

    Re-enabling NPM installing

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -8,14 +8,14 @@ exec > >(tee -a /workspaces/postCreate.log) 2>&1
 export PS4='+ [${BASH_SOURCE##*/}:${LINENO}] '
 set -x
 
-# echo "Installing npm"
-# sudo apt-get update
-# sudo apt-get install -y nodejs npm
-
 echo "Initializing Butano and Emulator.js submodules"
 git submodule update --init --recursive
 
-# echo "NPM install for Emulator.js"
-# npm install --prefix third_party/EmulatorJS
+# echo "Installing npm"
+sudo apt-get update
+sudo apt-get install -y nodejs npm
+
+echo "NPM install for Emulator.js"
+npm install --prefix third_party/EmulatorJS
 
 echo "Finished postCreate.sh"
