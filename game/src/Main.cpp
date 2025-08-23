d158958aea93399047c87544c2a4fd8b263239d9
alope107 <alope107@users.noreply.github.com>
2025-08-23 02:19:24 +0000

    Made game interactive

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -9,7 +9,21 @@ int main() {
 
     auto dot = bn::sprite_items::dot.create_sprite(0, 0);
 
+    bn::fixed speed = 1.5;
+
     while(true) {
+        if(bn::keypad::left_held()) {
+            dot.set_x(dot.x() - speed);
+        }
+        if(bn::keypad::right_held()) {
+            dot.set_x(dot.x() + speed);
+        }
+        if(bn::keypad::up_held()) {
+            dot.set_y(dot.y() - speed);
+        }
+        if(bn::keypad::down_held()) {
+            dot.set_y(dot.y() + speed);
+        }
         bn::core::update();
     }
 }
