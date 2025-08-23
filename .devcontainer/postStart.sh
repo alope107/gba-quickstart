a47417aa7a8bb71743a87b1c2ab498d5248ef018
alope107 <alope107@users.noreply.github.com>
2025-08-22 17:31:46 -0700

    Comment out postStart

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-echo "Starting EmulatorJS server..."
-nohup npm start --prefix third_party/EmulatorJS > /workspaces/emulatorjs.log 2>&1 &
+# echo "Starting EmulatorJS server..."
+# nohup npm start --prefix third_party/EmulatorJS > /workspaces/emulatorjs.log 2>&1 &
