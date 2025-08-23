2deda3b9954bb648dd6819ebc5aa74b3feb2bb09
alope107 <alope107@users.noreply.github.com>
2025-08-23 10:40:36 +0000

    Add notes about expra in postCreate

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -15,3 +15,8 @@ echo "Installing xpra"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xpra
 
 echo "Finished postCreate.sh"
+
+# xpra command in progress.
+# TODO: Make this either autostart or be something the user can easily start
+# Also debug issue with libresprite "installation"
+# xpra start :100 --bind-tcp=0.0.0.0:14500 --html=on     --start-child="xeyes" --no-daemon
