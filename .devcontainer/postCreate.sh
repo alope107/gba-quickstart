79f051726e5b0389dac159212a1292f9fe011393
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-08-28 19:38:52 +0000

    Clean up logging for postCreate/postStart

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,13 +3,7 @@ set -Eeuo pipefail
 
 echo "Beginning postCreate.sh"
 
-# Log for postCreate script
-exec > >(tee -a /workspaces/postCreate.log) 2>&1
-export PS4='+ [${BASH_SOURCE##*/}:${LINENO}] '
-set -x
-
 echo "Initializing Butano submodule"
 git submodule update --init --recursive
-
 
 echo "Finished postCreate.sh"
