e411911e7f278c499c170d7377f1cc113ceba82a
alope107 <alope107@users.noreply.github.com>
2025-08-28 03:02:26 +0000

    Fix xpra under qemu

--- .devcontainer/xpraserver.sh
+++ .devcontainer/xpraserver.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Start LibreSprite in xpra
+# Disable MIT-SHM for Xvfb (causes issues under qemu on amd64)
+# TODO: Investigate whether it's worth probing for MIT-SHM failure before falling back
 xpra start :100  \
      --bind-tcp=0.0.0.0:14500 \
      --html=on \
@@ -12,4 +15,5 @@ xpra start :100  \
      --pulseaudio=off \
      --webcam=off \
      --mdns=off \
+     --xvfb="Xvfb +extension Composite -nolisten tcp -noreset -extension MIT-SHM" \
      --start-child="/tools/libresprite/AppRun"
