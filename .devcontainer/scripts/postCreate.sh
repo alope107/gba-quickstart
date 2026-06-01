8844171f7c869c77ee78fdffc46ab029fa4ab86d
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-06-01 13:47:52 -0700

    Strip out old emulator, Dockerfile, and sprite editor and switch to extension-based approach

--- .devcontainer/scripts/postCreate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-echo "Beginning postCreate.sh"
-
-echo "Initializing Butano submodule"
-git submodule update --init --recursive
-
-echo "Finished postCreate.sh"
